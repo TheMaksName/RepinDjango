17b98d4bfa235d1466a9bef8a73ada64bdb666a0
TheMaksName <149165905+TheMaksName@users.noreply.github.com>
2025-04-15 12:02:44 +0400

    Update settings.py

--- registration/registration/settings.py
+++ registration/registration/settings.py
@@ -147,13 +147,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = '/static/'
-#  STATIC_ROOT = '/app/registration/staticfiles'  # Папка, куда собирается статика
-#  STATICFILES_DIRS = [
-#      '/app/registration/reg/static',  # Папка с твоими статическими файлами
-# ]
+ STATIC_ROOT = '/app/registration/staticfiles'  # Папка, куда собирается статика
+ STATICFILES_DIRS = [
+     '/app/registration/reg/static',  # Папка с твоими статическими файлами
+]
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_DIRS = os.path.join(BASE_DIR, 'static')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# STATIC_DIRS = os.path.join(BASE_DIR, 'static')
 
 
 # Default primary key field type
