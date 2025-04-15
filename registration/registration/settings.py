dd8aa990992ba19bcff7771ffc37a5d476853759
TheMaksName <149165905+TheMaksName@users.noreply.github.com>
2025-04-15 12:10:16 +0400

    Update settings.py

--- registration/registration/settings.py
+++ registration/registration/settings.py
@@ -147,8 +147,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = '/static/'
- STATIC_ROOT = '/app/registration/staticfiles'  # Папка, куда собирается статика
- STATICFILES_DIRS = [
+STATIC_ROOT = '/app/registration/staticfiles'  # Папка, куда собирается статика
+STATICFILES_DIRS = [
      '/app/registration/reg/static',  # Папка с твоими статическими файлами
 ]
 
